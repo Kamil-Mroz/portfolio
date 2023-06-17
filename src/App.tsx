39475cc831ebd555d16a95c8945e5245dc81fde2
Kamil Mróz <kamilmp17@gmail.com>
2023-06-17 18:54:38 +0200

    reset

--- src/App.tsx
+++ src/App.tsx
@@ -7,28 +7,7 @@ const router = createBrowserRouter([
     path: '/',
     element: <RootLayout />,
     errorElement: <ErrorPage />,
-    children: [
-      {
-        path: 'about',
-        element: <h2>About</h2>,
-      },
-      {
-        path: 'resume',
-        element: <h2>Resume</h2>,
-      },
-      {
-        path: 'portfolio',
-        element: <h2>Portfolio</h2>,
-      },
-      {
-        path: 'blog',
-        element: <h2>Blog</h2>,
-      },
-      {
-        path: 'contact',
-        element: <h2>Contact</h2>,
-      },
-    ],
+    children: [],
   },
 ])
 
--- src/routes/RootLayout.tsx
+++ src/routes/RootLayout.tsx
@@ -1,19 +1,9 @@
 import { Outlet } from 'react-router-dom'
-import Header from '../layouts/Header'
-import Sidebar from '../layouts/Sidebar'
-import { Card, CardContent, CardHeader } from '../components/ui/Card'
+
 const RootLayout = () => {
   return (
-    <div className="min-h-screen md:py-11 py-8 px-6 lg:px-16 md:flex gap-7">
-      <Sidebar />
-      <Card className="flex-1">
-        <CardHeader className="py-0 pr-0 mb-9">
-          <Header />
-        </CardHeader>
-        <CardContent>
-          <Outlet />
-        </CardContent>
-      </Card>
+    <div>
+      <Outlet />
     </div>
   )
 }
