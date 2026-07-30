401dffda26f46eed5712ecf2446354b9e93b9cde
Kamil-Mroz <kamilmp17@gmail.com>
2026-07-30 22:33:51 +0200

    test update file import

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -3,8 +3,8 @@ import useEmblaCarousel, {
 	type UseEmblaCarouselType,
 } from "embla-carousel-react";
 import * as React from "react";
+import { Button } from "@/components/ui/Button";
 import { cn } from "@/lib/utils";
-import { Button } from "./button";
 
 type CarouselApi = UseEmblaCarouselType[1];
 type UseCarouselParameters = Parameters<typeof useEmblaCarousel>;
