1c63f2844ffe2e99b0c998d0fde9d0318afcfea8
Kamil-Mroz <kamilmp17@gmail.com>
2026-07-30 22:18:51 +0200

    fix: resolve import path for carousel

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,19 @@
-import tailwindcss from "@tailwindcss/vite";
-import { devtools } from "@tanstack/devtools-vite";
+import { defineConfig } from 'vite'
+import { devtools } from '@tanstack/devtools-vite'
 
-import { tanstackRouter } from "@tanstack/router-plugin/vite";
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
-import viteReact from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import viteReact from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 const config = defineConfig({
-	plugins: [
-		devtools(),
-		tsconfigPaths({ projects: ["./tsconfig.json"] }),
-		tailwindcss(),
-		tanstackRouter({ target: "react", autoCodeSplitting: true }),
-		viteReact(),
-	],
-});
+  resolve: { tsconfigPaths: true },
+  plugins: [
+    devtools(),
+    tailwindcss(),
+    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+    viteReact(),
+  ],
+})
 
-export default config;
+export default config
